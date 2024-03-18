45dd7538cb670d44f058c0b65799ff1dba24a65b
Tomás <liamouzzz@gmail.com>
2024-03-18 18:58:04 +0000

    Continuação de gráficos
    Co-Authored-By: JoaoNuno2005 <146328858+JoaoNuno2005@users.noreply.github.com>

--- Projeto/Graficos.py
+++ Projeto/Graficos.py
@@ -9,6 +9,14 @@ def scatter_landsize_price():
     plt.xlabel('Landsize')
     plt.ylabel('Price')
     plt.show()
-scatter_landsize_price()
+#scatter_landsize_price()
 
-#funcao para mostrar grafico
+#funcao para mostrar grafico  sobre o preco mais elevado dos diferentes ficheiros onde no eixo x temos o nome do ficheiro e no eixo y o preco:
+def bar_max_price():
+    max_price = ficheiro_analise.groupby('source').max()['price']
+    max_price.plot(kind='bar')
+    plt.title('Max Price')
+    plt.xlabel('Source')
+    plt.ylabel('Price')
+    plt.show()
+bar_max_price()
